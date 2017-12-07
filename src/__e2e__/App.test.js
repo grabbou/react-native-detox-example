f2793bc722b3cdd8c98c88fbe48b048fdda4b77f
Mike Grabowski <grabbou@gmail.com>
2017-12-06 16:00:45 -0800

    Wrap it up

--- src/__e2e__/App.test.js
+++ src/__e2e__/App.test.js
@@ -6,6 +6,6 @@ describe('Main', () => {
 	it('should have welcome heading', async () => {
 		const text = element(by.id('welcome'));
 
-    await expect(text).toHaveText('Welcome to React Native');
+    await expect(text).toHaveText('Welcome to React Native!');
 	});
 })
